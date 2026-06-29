e7f9e247d2d5c7776215c4bf19e917766ea77f37
Marcos Ruiz Diaz <marcosezequielruizdiaz@gmail.com>
2026-06-29 12:48:34 -0300

    Estructura inicial: Implementación de metaclase Singleton, decorador log y clases base Persona, Usuario y Libro

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sistema de Gestión de Biblioteca Digital
+
+## Descripción
+Este es el Trabajo Práctico Final de la Unidad I para la materia Programación Avanzada (189) de la Universidad Nacional Guillermo Brown. El sistema permite administrar libros, usuarios y préstamos utilizando Programación Orientada a Objetos en Python.
+
+## Integrantes del Grupo
+* Marcos Ruiz Diaz
+* Leandro Rodriguez
+
+## Instrucciones para ejecutar el proyecto
+1. Clonar este repositorio.
+2. Ejecutar el archivo `main.py` desde la terminal.
--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("python funciona")
+import datetime
+
+class SingletonMeta(type):
+    _instancias_ = {}
+    
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instancias:
+            #si la clase no fue creada antes, la crea y lo guarda
+            cls._instancias[cls] = super().__call__(*args, **kwargs)
+        return cls._instancias[cls]
+
+#decorador propio, lo usamos para avisa en la terminal cada vez que se realice una accion importante
+def log_operacion(funcion):
+    def wrapper(*args, **kwargs):
+        print(f"\n[LOG] Ejecutando: {funcion.__name__.upper()}...")
+        resultado = funcion(*args, **kwargs)
+        print("[LOG] accion finalizada con exito.")
+        return resultado
+    return wrapper
+
+#Modelo de datos
+class persona:
+    def __init__(self, nombre, apellido, dni):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+    
+    def mostrar_info(self):
+        return f"{self.nombre} {self.apellido} (DNI: {self.dni})"
+
+class Usuario(persona):
+    def __init__(self, nombre, apellido, dni, correo):
+        super().__init__(nombre, apellido, dni)
+        self.correo = correo
+    
+    def mostrar_info(self):
+        return f"Usuario: {self.nombre} {self.apellido} | DNI: {self.dni} | Email: {self.correo}"
+
+class Libro:
+    def __init__(self, titulo, autor, isbn, año, paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.año = año
+        self.paginas = paginas
+        self.prestado = False 
+    
+    def mostrar_info(self):
+        estado = "PRESTADO" if self.prestado else "DISPONIBLE"
+        return f"Libro: '{self.titulo}' - Autor: {self.autor} (ISBN: {self.isbn}) [{estado}]"
